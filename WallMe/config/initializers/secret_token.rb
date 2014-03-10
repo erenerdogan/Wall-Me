a27d3564421baeaf69c17e6e49ad291685d4c312
Eren <eren@Eren-MacBook-Air.local>
2014-03-10 05:05:12 +0200

    Proje Başlangıç
    WallMe Projesine Başlandı.

---
+++ WallMe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WallMe::Application.config.secret_key_base = '02dcf4ae354788cd64287d4be203c21cb4433f9d67a6bd0ff57d4732c6741fd14865b5857822d89187bea9cb0e56e18401a5aa30930c562a2e68e0c6b5ace0d0'
---
+++ WallMe/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WallMe::Application.config.session_store :cookie_store, key: '_WallMe_session'
